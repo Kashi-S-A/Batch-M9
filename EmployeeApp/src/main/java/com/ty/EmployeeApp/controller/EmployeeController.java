c961b4c276ffee7e68115be7fee45e74efb0352a
Kashi-S-A <kashappaappannavar@gmail.com>
2024-12-27 13:23:03 +0530

    second save

--- EmployeeApp/src/main/java/com/ty/EmployeeApp/controller/EmployeeController.java
+++ EmployeeApp/src/main/java/com/ty/EmployeeApp/controller/EmployeeController.java
@@ -1,12 +1,17 @@
 package com.ty.EmployeeApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ty.EmployeeApp.dto.EmployeeRequest;
+import com.ty.EmployeeApp.dto.EmployeeResponse;
+import com.ty.EmployeeApp.responsestruct.ResponseStructure;
 import com.ty.EmployeeApp.service.EmployeeService;
 
 @RestController
@@ -15,10 +20,28 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
-	
+
 	@PostMapping("/save")
-	public String saveEmp(@RequestBody EmployeeRequest employeeRequest) {
-		
+	public ResponseEntity<?> saveEmployee(@RequestBody EmployeeRequest employeeRequest) {
 		return employeeService.saveEmp(employeeRequest);
+	}
+	
+	@PostMapping("/saves")
+	public ResponseEntity<String> saveEmp(@RequestBody EmployeeRequest employeeRequest) {
+		return employeeService.saveEm(employeeRequest);
+	}
+	
+	@GetMapping("/get")
+	public ResponseEntity<ResponseStructure<EmployeeResponse>> getEmployee(@RequestParam Integer eid) {
+		return employeeService.getEmp(eid);
+	}
+	
+	@GetMapping("/test")
+	public ResponseEntity<String> getEmp() {
+		
+		EmployeeRequest em=null;
+		em.getEmail();
+		
+		return ResponseEntity.ok("Successfull");
 	}
 }
